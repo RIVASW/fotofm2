fc9facbff39003553efd2b15501efc8daaf886b9
Ivan Romanyuk <ivanrom@sibtelcom.ru>
2016-08-24 21:14:15 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fotofm2::Application.config.secret_key_base = 'efffab2b8097efaf29f34ce59bb35901abf9ded4cd42d69e23edacac5f3b618358b32beaaad936a5863ab74c7ba474dc2baa739f0542af98f2255b10a8905128'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fotofm2::Application.config.session_store :cookie_store, key: '_fotofm2_session'
